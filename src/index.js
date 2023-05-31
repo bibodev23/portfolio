5794c56d78bbcf3e85916ab21950cb4cf2c460b9
bibodev23 <bibodev23@gmail.com>
2023-05-31 18:33:00 +0200

    responsive smartphone

--- src/index.js
+++ src/index.js
@@ -72,3 +72,12 @@ function apparition(){
     }
 }
 
+let navLinks = document.querySelector(".navNarrow");
+let narrowLinks = document.querySelector(".narrowLinks");
+
+
+navLinks.addEventListener("click", toggle);
+
+function toggle() {
+    narrowLinks.classList.toggle("hidden")
+}
